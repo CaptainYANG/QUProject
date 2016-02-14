8bdf710ab480b6d29c53cc0d66f2f446b763fd42
LIU Ziqi <sushill647@gmail.com>
2016-02-14 23:06:11 +0100

    add score with song obj

--- core/src/com/mygdx/rhythm/FileChooser.java
+++
@@ -1,7 +0,0 @@
-package com.mygdx.rhythm;
-
-/**
- * Created by sushi on 10/02/16.
- */
-public class FileChooser {
-}
---
+++ core/src/com/mygdx/rhythm/Songs.java
@@ -0,0 +1,7 @@
+package com.mygdx.rhythm;
+
+/**
+ * Created by sushi on 14/02/16.
+ */
+public class Songs {
+}
